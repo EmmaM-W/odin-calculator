712a211da0d52a75e084f11d8319db0e1b6c461e
EmmaM-W <133997936+EmmaM-W@users.noreply.github.com>
2023-05-28 01:10:54 +0100

    add operate function

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ function multiply (x,y){
 function divide (x,y){
     return x/y;
 }
+
+function operate (x,operation,y){
+    if (operation == '+') {
+        return add(x,y);
+    }
+    if (operation == '-') {
+        return subtract(x,y);
+    }
+    if (operation == '*') {
+        return multiply(x,y);
+    }
+    if (operation == '/') {
+        return divide(x,y);
+    }
+}
